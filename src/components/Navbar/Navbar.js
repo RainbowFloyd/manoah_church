0bde85aaef707ef730b945264b0c81f9be233431
Adam <rainbowfloyd911@gmail.com>
2019-03-04 05:50:29 -0500

    added home tab

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavbarItem from '../NavbarItem/NavbarItem';
 
 const Navbar = (props) => {
-	const tabs = ['Staff', 'Connect', 'Media', 'Prayer'];
+	const tabs = ['Home', 'Staff', 'Connect', 'Media', 'Prayer'];
 	const tabItems = (
 		tabs.map((tab, index) => {
 			return <NavbarItem tab={tab} />
