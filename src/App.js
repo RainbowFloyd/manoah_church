a22623ce7d8d2741b61b88ad44dd06bb913f4081
Adam <rainbowfloyd911@gmail.com>
2019-03-04 06:01:38 -0500

    Added route to home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Wip from './components/Wip/Wip';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Wip />
+        <Route path='/' component={Wip} />
       </div>
     );
   }
