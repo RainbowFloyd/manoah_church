60a43b55e1c1f1158372eefbb9103ad44d1be7ff
Adam <rainbowfloyd911@gmail.com>
2019-03-04 06:53:21 -0500

    added media component and made route for it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Wip from './components/Wip/Wip';
 import Staff from './components/Staff/Staff'
 import Connect from './components/Connect/Connect'
+import Media from './components/Media/Media'
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <Navbar />
         <Route path='/staff' component={Staff} />
         <Route path='/connect' component={Connect} />
+        <Route path='/media' component={Media} />
         <Route path='/' exact component={Wip} />
       </div>
     );
